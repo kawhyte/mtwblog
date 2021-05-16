46ba45c7dabf7758a2f29462804271b5b8b1ec77
Kenny Whyte <kawhyte@gmail.com>
2021-05-16 15:11:18 -0700

    rating

--- components/pro-con-list.js
+++ components/pro-con-list.js
@@ -12,7 +12,7 @@ function ProConList({ positives, negatives ,verdict }) {
 							<div className='w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 flex-shrink-0'>
 								<svg
 									xmlns='http://www.w3.org/2000/svg'
-									classNameName='h-8 w-8'
+									className='h-8 w-8'
 									viewBox='0 0 20 20'
 									fill='currentColor'>
 									<path d='M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z' />
@@ -23,17 +23,17 @@ function ProConList({ positives, negatives ,verdict }) {
 									What we loved
 								</h2>
 
-								<div classNameName='flex flex-col sm:items-start sm:text-left  items-center -mb-1 space-y-2.5'>
+								<div className='flex flex-col sm:items-start sm:text-left  items-center -mb-1 space-y-2.5'>
 									<ul>
 										{positives?.map((positive, index) => (
-											<li key={index} classNameName="my-3 leading-relaxed">
+											<li key={index} className="my-3 leading-relaxed">
 												<span className='bg-green-100 text-green-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center'>
 													<svg
 														fill='none'
 														stroke='currentColor'
-														stroke-linecap='round'
-														stroke-linejoin='round'
-														stroke-width='3'
+														strokeLinecap='round'
+														strokeLinejoin='round'
+														strokeWidth='3'
 														className='w-3 h-3'
 														viewBox='0 0 24 24'>
 														<path d='M20 6L9 17l-5-5'></path>
@@ -52,7 +52,7 @@ function ProConList({ positives, negatives ,verdict }) {
 							<div className='w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-gray-100 text-gray-500 flex-shrink-0'>
 								<svg
 									xmlns='http://www.w3.org/2000/svg'
-									classNameName='h-8 w-8'
+									className='h-8 w-8'
 									viewBox='0 0 20 20'
 									fill='currentColor'>
 									<path d='M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z' />
@@ -63,14 +63,14 @@ function ProConList({ positives, negatives ,verdict }) {
 									What we didnt like{" "}
 								</h2>
 
-								<div classNameName='flex flex-col sm:items-start sm:text-left  items-center -mb-1 space-y-2.5'>
+								<div className='flex flex-col sm:items-start sm:text-left  items-center -mb-1 space-y-2.5'>
 									<ul>
 										{negatives?.map((negative, index) => (
-											<li key={index} classNameName="my-3 leading-relaxed">
+											<li key={index} className="my-3 leading-relaxed">
 												<span className='bg-gray-100 text-gray-500 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center'>
 													<svg
 														xmlns='http://www.w3.org/2000/svg'
-														classNameName='h-3 w-3'
+														className='h-3 w-3'
 														viewBox='0 0 20 20'
 														fill='currentColor'>
 														<path
@@ -94,7 +94,7 @@ function ProConList({ positives, negatives ,verdict }) {
 				<div className='w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-yellow-100 text-yellow-500 flex-shrink-0'>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
-						classNameName='h-8 w-8'
+						className='h-8 w-8'
 						fill='none'
 						viewBox='0 0 24 24'
 						stroke='currentColor'>
@@ -111,8 +111,8 @@ function ProConList({ positives, negatives ,verdict }) {
 						Verdict
 					</h2>
 
-					<div classNameName='flex flex-col sm:items-start sm:text-left leading-relaxed  items-center -mb-1 space-y-2.5'>
-						<p classNameName="font-sans">
+					<div className='flex flex-col sm:items-start sm:text-left leading-relaxed  items-center -mb-1 space-y-2.5'>
+						<p className="font-sans">
 						{verdict}
 						</p>
 					</div>
