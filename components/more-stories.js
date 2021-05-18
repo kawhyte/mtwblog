8e468b32d9d18ffec1d51397395fa00af00bb0f7
Kenny Whyte <kawhyte@gmail.com>
2021-05-17 19:41:41 -0700

    grid

--- components/more-stories.js
+++ components/more-stories.js
@@ -7,7 +7,7 @@ export default function MoreStories({ posts }) {
       <h2 className="mb-8 text-4xl md:text-6xl font-bold tracking-tighter leading-tight">
         More Adventures and Reviews
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2   md:grid-cols-3 gap-16 px-2 py-2 mx-2 my-1 bg-bgcolor rounded-lg">
+      <div className="grid grid-cols-1 sm:grid-cols-1  md:grid-cols-2 gap-16 px-2 py-2 mx-2 my-1 bg-bgcolor rounded-lg">
         {posts.map((post) => (
           <Section
             key={post.slug}
--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,63 @@
-import Container from '../components/container'
-import Section from '../components/section'
-import Welcome from '../components/welcome'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from "../components/container";
+import Section from "../components/section";
+import Welcome from "../components/welcome";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
 
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+	const heroPost = allPosts[0];
+	const morePosts = allPosts.slice(1);
 
-  return (
-    <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Meet the Whytes by {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-             // date={heroPost.date}
-              //author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
+	return (
+		<>
+			<Layout preview={preview}>
+				<Head>
+					<title>Travel and Food Reviews by {CMS_NAME}</title>
+				</Head>
+				<Container>
+					<Intro />
+					{heroPost && (
+						<HeroPost
+							title={heroPost.title}
+							coverImage={heroPost.coverImage}
+							// date={heroPost.date}
+							//author={heroPost.author}
+							slug={heroPost.slug}
+							excerpt={heroPost.excerpt}
+						/>
+					)}
 
-          {morePosts.length > 0 && 
+					{morePosts.length > 0 &&
+						morePosts.map((post) => {
+							<Section
+								key={post.slug}
+								title={post.title}
+								coverImage={post.coverImage}
+								date={post.date}
+								author={post.author}
+								slug={post.slug}
+								excerpt={post.excerpt}
+								webLink={post.webLink}
+							/>;
+						})}
 
-            morePosts.map((post) => {
-                  <Section 
-                  
-                  key={post.slug}
-                      title={post.title}
-                      coverImage={post.coverImage}
-                      date={post.date}
-                      author={post.author}
-                      slug={post.slug}
-                      excerpt={post.excerpt}
-                      webLink={post.webLink}
-                  
-                  />  
-                
-                
-                } )}
-
-
-
-
-
-
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-       
-          
-       
-          </Container><Welcome />
-      </Layout>
-    </>
-  )
+					{morePosts.length > 0 && <MoreStories posts={morePosts} />}
+				</Container>
+				<Welcome />
+			</Layout>
+		</>
+	);
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
-  return {
-    props: { allPosts, preview },
-    revalidate: 1
-  }
+	const allPosts = await getAllPostsForHome(preview);
+	return {
+		props: { allPosts, preview },
+		revalidate: 1,
+	};
 }
