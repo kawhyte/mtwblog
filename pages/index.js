89c07f059efec21e951d22ac547b62b07d7ac440
Kenny Whyte <kawhyte@gmail.com>
2021-05-17 19:38:12 -0700

    .

--- pages/index.js
+++ pages/index.js
@@ -58,9 +58,9 @@ export default function Index({ allPosts, preview }) {
 
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
        
-          <Welcome />
+          
        
-          </Container>
+          </Container><Welcome />
       </Layout>
     </>
   )
