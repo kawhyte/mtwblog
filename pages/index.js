4f36fa45122ee36f38e1169de5558a300bf243a1
Kenny Whyte <kawhyte@gmail.com>
2021-05-19 09:06:59 -0700

    .

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Index({ allPosts, preview }) {
 								More Reviews
 							</h2>
 <Link href="/allposts"> 
-							<button className='px-2 py-2 font-medium tracking-wide text-xl text-black capitalize transition-colors duration-200 transform  rounded-md dark:bg-gray-800 hover:underline dark:hover:bg-gray-700 focus:outline-none '>
+							<button className='px-2 py-2 font-medium tracking-wide text-xl text-indigo-500 capitalize transition-colors duration-200 transform  rounded-md dark:bg-gray-800 hover:underline dark:hover:bg-gray-700 focus:outline-none '>
 								View All Reviews
 							</button></Link>
 						</div>
