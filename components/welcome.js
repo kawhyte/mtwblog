5a1e7918a50e29682064a74522a5a33b30842869
Kenny Whyte <kawhyte@gmail.com>
2021-05-22 13:27:36 -0700

    spacing

--- components/welcome.js
+++ components/welcome.js
@@ -4,7 +4,7 @@ function Welcome() {
 	return (
 		<div className='bg-indigo-50 pattern'>
 			<section className='text-gray-600 body-font'>
-				<div className='container px-5 py-12 mb-12 mx-auto'>
+				<div className='container px-5 py-2 md:py-12 mb-12 mx-auto'>
 					<div className='font-playfair-display flex flex-col py-24 text-center w-full mb-10'>
 						<h2 className='mb-8  text-4xl md:text-6xl font-bold tracking-tighter leading-tight'>
 							Hi! We are the Whytes. <br></br> Welcome to our space on the{" "}
