be8a5af781c155df77cc39d1103d6b9d65c45a2d
Kenny Whyte <kawhyte@gmail.com>
2021-06-07 14:32:11 -0700

    .

--- pages/hotel.js
+++ pages/hotel.js
@@ -3,7 +3,7 @@ import Section from "../components/section";
 import ReviewHeader from "../components/review-header";
 import MoreStories from "../components/more-stories";
 import Layout from "../components/layout";
-import { getAllPostsForHome } from "../lib/api";
+import { getAllPostsForHome,getAllPostsForCategory } from "../lib/api";
 import Head from "next/head";
 import { CMS_NAME, HOTEL } from "../lib/constants";
 
@@ -47,7 +47,7 @@ function AllHotels({ allPosts, preview }) {
 }
 
 export async function getStaticProps({ preview = false   }) {
-	const allPosts = await getAllPostsForHome(preview);
+	const allPosts = await getAllPostsForCategory(preview, HOTEL);
 	return {
 		props: { allPosts, preview },
 		revalidate: 1,
