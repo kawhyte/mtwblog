a9e501388548f1f1733bea10202121d620a15742
Kenny Whyte <kawhyte@gmail.com>
2021-07-02 10:53:32 -0700

    .

--- studio/schemas/img.js
+++ studio/schemas/img.js
@@ -10,7 +10,7 @@ export default {
           {
             name: 'alt',
             type: 'string',
-            title: 'Alternative text',
+            title: 'Image Caption',
           },
         ],
       }
