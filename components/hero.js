9775a2e10f0c06206ed870ddf3547d9cbe47058b
Kenny Whyte <kawhyte@gmail.com>
2021-07-31 20:27:40 -0700

    removed blur

--- components/hero.js
+++ components/hero.js
@@ -9,7 +9,7 @@ function Hero() {
 			<div className="md:hidden">
 				<Image
 					blurDataURL='https://res.cloudinary.com/babyhulk/image/upload/e_pixelate:200,q_30/v1627353251/hero-image/PXL_20210613_032035183.MP_1.webp'
-					placeholder='blur'
+					
 					src='https://res.cloudinary.com/babyhulk/image/upload/v1627353251/hero-image/PXL_20210613_032035183.MP_1.webp'
 					className='block absolute  h-full w-full object-cover'
 					aria-label='Justify'
