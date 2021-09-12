4a67fb837c444addd11a508b187faa41d7924aa6
Kenny Whyte <kawhyte@gmail.com>
2021-09-12 14:45:42 -0700

    updated block videos

--- components/post-body.js
+++ components/post-body.js
@@ -1,24 +1,45 @@
-import markdownStyles from './markdown-styles.module.css'
-import BlockContent from '@sanity/block-content-to-react'
-import getYouTubeId from 'get-youtube-id'
-import YouTube from 'react-youtube'
+import markdownStyles from "./markdown-styles.module.css";
+import BlockContent from "@sanity/block-content-to-react";
+import getYouTubeId from "get-youtube-id";
+import YouTube from "react-youtube";
+import ReactPlayer from "react-player/youtube";
 
-const serializers =  {
-  
-  types: {
-    youtube2: ({node}) => {
-      const { url } = node
-      const id = getYouTubeId(url)
-      return (<YouTube videoId={id} className="bg-green-200  p-2"/>)
-    }
-  }
-}
+const serializers = {
+	types: {
+		youtube2: ({ node }) => {
+			// console.log(node);
+			// const { url } = node;
+			// const id = getYouTubeId(url);
+			return (
+				<div className='mb-12 md:mb-24 -mx-5 sm:mx-0  bg-indigo-300'>
+					<div className='player-wrapper'>
+						<ReactPlayer
+							className='react-player'
+							url={node.url}
+							width={"100%"}
+							height={"100%"}
+							controls={true}
+							loop
+							muted
+						/>
+					</div>
+				</div>
+			);
+		},
+	},
+};
 
 export default function PostBody({ content }) {
-  return (
-    <div className="max-w-4xl  text-justify mx-4 my-10">
-      <BlockContent blocks={content} serializers={serializers} projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID} dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}  className={markdownStyles.markdown} />
-    </div>
-  )
+	return (
+		<div className='max-w-4xl  text-justify mx-4 my-10'>
+			<BlockContent
+				blocks={content}
+				serializers={serializers}
+				projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+				dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+				className={markdownStyles.markdown}
+			/>
+		</div>
+	);
 }
 serializers.types.youtube2.displayName = "serializers";
