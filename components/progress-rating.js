461e4da22c72b67e21e9f31ccccd7f995dca451e
Kenny Whyte <kawhyte@gmail.com>
2021-09-16 12:04:09 -0700

    .

--- components/progress-rating.js
+++ components/progress-rating.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function ProgressRating({progress }) {
-    let value = `w-${Math.floor(progress)}/5 `
+    let value = `h-full text-center text-xs text-white bg-green-400 rounded-full w-${Math.floor(progress)}/5 `
 
     console.log(" value", value)
     return (
         <div className="flex flex-row justify-center items-center rounded-lg w-52 ">
             <div className="w-full h-2 bg-gray-200 rounded-full">
-                <div className={"h-full text-center text-xs text-white bg-green-400 rounded-full  " + value } >
+                <div className={ value } >
                 </div>
                 </div>
                 <span className='text-sm ml-3 '>{progress} </span>
