d810d455874160d0a22e4ad1143748bcfa4ced4f
Kenny Whyte <kawhyte@gmail.com>
2021-09-18 11:44:56 -0700

    test progressbar

--- components/progress-rating.js
+++ components/progress-rating.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function ProgressRating({progress }) {
-    let value = `h-full text-center text-xs text-white bg-green-400 rounded-full w-${Math.floor(progress)}/5 `
+function ProgressRating({ progress }) {
+	
+	//console.log(" value", (progress / 5) * 100);
+	let progressPercentage = (Math.floor(progress) / 5) * 100;
 
-    //console.log(" value", value)
-    return (
-        <div className="flex flex-row justify-center items-center rounded-lg w-52 ">
-            <div className="w-full h-2 bg-gray-200 rounded-full">
-                <div className={ value } >
-                </div>
-                </div>
-                <span className='text-sm ml-3 '>{progress} </span>
-        </div>
-    )
+	return (
+		<>
+			<div className='h-1 w-full bg-gray-300 <div className="flex flex-row justify-center items-center rounded-lg ">'>
+				<div
+					style={{ width: `${progressPercentage}%` }}
+					className={`h-full ${
+						progressPercentage < 50 ? "bg-red-600" : "bg-green-600"
+					}`}></div>
+			</div>
+			<span className='text-sm ml-3 '>{progress} </span>
+		</>
+	);
 }
 
-export default ProgressRating
+export default ProgressRating;
