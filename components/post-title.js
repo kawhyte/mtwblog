0a089dd508d40395d7f70d8f1a4a8b8c47efbe9f
Kenny Whyte <kawhyte@gmail.com>
2021-09-19 18:30:09 -0700

    .

--- components/post-title.js
+++ components/post-title.js
@@ -2,7 +2,7 @@ export default function PostTitle({ children }) {
   return (
     <>
    
-    <h1 className="py-2  md:py-2 z-10 font-fancy text-2xl md:text-3xl lg:text-5xl  font-semibold tracking-tighter leading-tight md:leading-none  text-center md:text-left">
+    <h1 className="py-2   md:py-2 z-10 font-fancy text-2xl md:text-3xl lg:text-5xl  font-semibold tracking-tighter leading-tight md:leading-none  text-center md:text-left">
       {children}
       
     </h1>
