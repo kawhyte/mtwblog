55002a5162349662365bed90fe076311ba2e3fb3
Kenny Whyte <kawhyte@gmail.com>
2021-10-20 10:33:35 -0700

    added story page

---
+++ components/story-section.js
@@ -0,0 +1,114 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { imageBuilder } from "../lib/sanity";
+import cn from "classnames";
+import StarRating from "./star-rating";
+import { add } from "date-fns";
+
+function StorySection(props) {
+	return (
+		<div className='grid grid-cols-1 gap-4 lg:gap-8  sm:grid-cols-2 grid-rows-1 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3   '>
+			{props.stories.map((item) => {
+				return (
+					<div key={item.slug}>
+						<section className='text-gray-600 body-font mb-10'>
+							<div className='rounded-xl h-full'>
+								{item.slug ? (
+									<Link as={`/posts/${item.slug}`} href='/posts/[slug]'>
+										<a aria-label={item.title}>
+											<Image
+												width={392}
+												height={171}
+												blurDataURL={imageBuilder(item.coverImage)
+													.width(392)
+													.height(171)
+													.quality(1)
+													.url()}
+												placeholder='blur'
+												alt={`Cover Image for ${item.title}`}
+												className={cn("shadow-small", {
+													"object-cover object-center h-full w-full": item.slug,
+												})}
+												src={imageBuilder(item.coverImage)
+													.width(392)
+													.height(171)
+													.url()}
+											/>
+										</a>
+									</Link>
+								) : (
+									image
+								)}
+							</div>
+							<h2 className=' text-xl font-medium title-font text-gray-700 mt-3'>
+								{item.title}
+							</h2>
+							<p className='text-base leading-relaxed mt-2'>{item.excerpt}</p>
+							<div className=' text-gray-500 block   mb-2  text-lg'>
+								
+								<Link as={`/posts/${item.slug}`} href='/posts/[slug]'>
+									<a className='text-green-800 bg-white inline-flex items-center mt-1'>
+										View Story
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											className='w-4 h-4 ml-2'
+											viewBox='0 0 24 24'>
+											<path d='M5 12h14M12 5l7 7-7 7'></path>
+										</svg>
+									</a>
+								</Link>
+							</div>
+						</section>
+					</div>
+				);
+			})}
+		</div>
+	);
+
+	{
+		/*<div key={i+1}>
+				<section className='text-gray-600 body-font mb-6'>
+					<div className='rounded-xl h-full overflow-hidden'>
+						{slug ? (
+							<Link as={`/posts/${slug}`} href='/posts/[slug]'>
+								<a aria-label={title}>{image}</a>
+							</Link>
+						) : (
+							image
+						)}
+					</div>
+					<h2 className='font-fancy text-xl font-medium title-font text-gray-900 mt-3'>
+						{title}
+					</h2>
+					<p className='text-base leading-relaxed mt-2'>{excerpt}</p>
+					<div className=' text-gray-500 block   mb-2  text-lg'>
+					{address ? address : "No address provided"}
+				</div>
+			
+
+					<Link as={`/posts/${slug}`} href='/posts/[slug]'>
+						<a className='text-green-800 bg-white inline-flex items-center mt-1'>
+							View Review
+							<svg
+								fill='none'
+								stroke='currentColor'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth='2'
+								className='w-4 h-4 ml-2'
+								viewBox='0 0 24 24'>
+								<path d='M5 12h14M12 5l7 7-7 7'></path>
+							</svg>
+						</a>
+					</Link>
+				</section>
+						</div>*/
+	}
+}
+
+export default StorySection;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Container from "../components/container";
 import Section from "../components/section";
+import StorySection from "../components/story-section";
 import Welcome from "../components/welcome";
 import MoreStories from "../components/more-stories";
 import HeroPost from "../components/hero-post";
@@ -17,8 +18,10 @@ import Navbar from "../components/navbar";
 export default function Index({ allPosts, allStories, preview }) {
 	const heroPost = allPosts[0];
 	const morePosts = allPosts.slice(1, 7);
+	const moreStories = allStories.slice(0, 7);
 
 	console.log("ALL allStories ", allStories)
+	console.log("ALL moreStories ", moreStories)
 
 	return (
 		<>
@@ -65,6 +68,32 @@ export default function Index({ allPosts, allStories, preview }) {
 								type='button'
 								className='py-2 px-4 bg-pink-500 hover:bg-pink-600 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg '>
 								All Hotel & Food Reviews
+							</button>
+						</Link>
+					</div>{" "}
+
+					{moreStories.length > 0 ? (
+					
+						<div className='flex flex-wrap w-full mb-10'>
+						<div className='lg:w-1/2 w-full mb-6 lg:mb-0 '>
+							<h1 className='font-fancy  sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900'>
+							More Stories
+							</h1>
+							<div className='h-1 w-20 bg-pink-500 rounded'></div>
+						</div>
+					</div>
+					) : (
+						""
+					)}
+					{moreStories.length > 0 && <StorySection stories={moreStories} />}
+					{/*morePosts.length > 0 && <MoreStories posts={morePosts} />*/}
+					<div className=' mx-auto container my-12 max-w-xs'>
+						<Link href='/allposts' passHref>
+							<button
+								aria-label='Justify'
+								type='button'
+								className='py-2 px-4 bg-pink-500 hover:bg-pink-600 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg '>
+								All Stories
 							</button>
 						</Link>
 					</div>{" "}
