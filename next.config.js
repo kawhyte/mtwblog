f520ba100c6bfa2cf06e797d45ddf49fc25e2f78
Kenny Whyte <kawhyte@gmail.com>
2021-11-10 15:29:25 -0800

    added PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
 	images: {
-		domains: ['cdn.sanity.io', 'res.cloudinary.com'],
+		domains: ["cdn.sanity.io", "res.cloudinary.com"],
 	},
-	
-};
+	pwa: {
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV === 'development',
+	},
+});
