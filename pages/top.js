54de4196e5f4fae0ea6637babdca991629313e04
Kenny Whyte <kawhyte@gmail.com>
2021-12-17 10:26:02 -0800

    err

--- pages/top.js
+++ pages/top.js
@@ -10,7 +10,7 @@ function top() {
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
               <div className="h-1 w-20 bg-pink-500 rounded"></div>
             </div>
-            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
           </div>
           <div className="flex flex-wrap -m-4">
             <div className="xl:w-1/4 md:w-1/2 p-4">
