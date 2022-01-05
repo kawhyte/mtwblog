3dcc3398a78738742bf3226833a6ed2eeb64c22e
Kenny Whyte <kawhyte@gmail.com>
2022-01-05 14:31:32 -0800

    .

--- components/room-tech.js
+++ components/room-tech.js
@@ -3,122 +3,195 @@ import React from "react";
 function RoomTech() {
 	return (
 		<section className=''>
+			<section className='text-gray-600 body-font overflow-hidden'>
+				<div className='container px-5 py-20 mx-auto'>
+					<div className='flex flex-col text-center w-full mb-12'>
+						<h1 className='font-fancy text-6xl md:text-5xl lg:text-5xl font-bold tracking-tighter leading-tight md:leading-none text-center md:text-left'>
+							Hotel Room Techology 
+						</h1>
+					
+					</div>
+					<div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+						<div className='p-4  w-full'>
+							<div className='h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden'>
+							
+							<span className="bg-indigo-500 text-white px-3 py-1 tracking-widest text-base absolute right-0 top-0 rounded-bl uppercase">Very Fast</span>
+							<h2 className='text-pink-500  text-base  tracking-widest title-font mb-1 font-medium uppercase'>
+									Internet Speed
+								</h2>
 
-        <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Technology</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.</p>
- 
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            
-            <div className="p-4  w-full">
-              <div className="h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden">
-                <h2 className="text-pink-500  text-base  tracking-widest title-font mb-1 font-medium uppercase">Internet Speed</h2>
-                <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                  <span>38</span>
-                  <span className="text-lg ml-1 font-normal text-gray-500">Mbps</span>
-                </h1>
-                <p className="mb-3">This internet speed is great for:</p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-6 h-6 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Browsing the Web
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-6 h-6 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Zoon Meetings 
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-6 h-6 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Streaming Music
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-6 h-6 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Streaming HD Movies
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-6 h-6 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Streaming 4k Movies
-                </p>
-            
-             
-              
-                
-                <p className="text-xs text-gray-500 mt-3">Literally you probably havent heard of them jean shorts.</p>
-              </div>
-            </div>
-            <div className="p-4  w-full">
-              <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
-                <h2 className="text-pink-500  text-base tracking-widest title-font mb-1 font-medium uppercase">Ports, Plugs & TV</h2>
-                <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                  
-                </h1>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Vexillologist pitchfork
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Tumeric plaid portland
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Hexagon neutra unicorn
-                </p>
-                <p className="flex items-center text-gray-600 mb-2">
-                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Vexillologist pitchfork
-                </p>
-                <p className="flex items-center text-gray-600 mb-6">
-                  <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" className="w-3 h-3" viewBox="0 0 24 24">
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Mixtape chillwave tumeric
-                </p>
-               
-                <p className="text-xs text-gray-500 mt-3">Literally you probably havent heard of them jean shorts.</p>
-              </div>
-            </div>
-            
-          </div>
-        </div>
-      </section>
+								<h1 className='text-5xl text-gray-900 leading-none flex items-start pb-4 mb-4 border-b border-gray-200'>
+									<span className="text-8xl">38</span>
+									<span className='text-lg ml-1 font-normal text-gray-500'>
+										Mbps
+									</span>
+								</h1>
+								
+								<p className='mb-3'>This internet speed is great for:</p>
+
+								<div className="grid grid-cols-2 lg:grid-cols-3 gap-3"> 
+								<p className='flex   items-center text-gray-600 mb-2'>
+									<span className='w-6 h-6 mb-2 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Web Browsing
+								</p>
+								<p className='flex   items-center text-gray-600 mb-2'>
+									<span className='w-6 h-6 mb-2 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Zoom Meetings
+								</p>
+								<p className='flex   items-center text-gray-600 mb-2'>
+									<span className='w-6 h-6 mb-2 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Streaming Music
+								</p>
+						
+						
+								<p className='flex   items-center text-gray-600 mb-2'>
+									<span className='w-6 h-6 mb-2 mr-2 inline-flex items-center justify-center bg-green-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Streaming Movies
+								</p>
+								
+								
+								
+								
+
+								
+							</div>
+							</div>
+						</div>
 
 
+                        
+						<div className='p-4  w-full'>
+							<div className='h-full p-6 rounded-lg border-2 border-indigo-500 flex flex-col relative overflow-hidden'>
+								<h2 className='text-pink-500  text-base tracking-widest title-font mb-1 font-medium uppercase'>
+									Ports, Plugs & TV
+								</h2>
+								<h1 className='text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200'></h1>
+								<p className='flex items-center text-gray-600 mb-2'>
+									<span className='w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Vexillologist pitchfork
+								</p>
+								<p className='flex items-center text-gray-600 mb-2'>
+									<span className='w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Tumeric plaid portland
+								</p>
+								<p className='flex items-center text-gray-600 mb-2'>
+									<span className='w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Hexagon neutra unicorn
+								</p>
+								<p className='flex items-center text-gray-600 mb-2'>
+									<span className='w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Vexillologist pitchfork
+								</p>
+								<p className='flex items-center text-gray-600 mb-6'>
+									<span className='w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0'>
+										<svg
+											fill='none'
+											stroke='currentColor'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2.5'
+											className='w-3 h-3'
+											viewBox='0 0 24 24'>
+											<path d='M20 6L9 17l-5-5'></path>
+										</svg>
+									</span>
+									Mixtape chillwave tumeric
+								</p>
 
-
-
-        
-			
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</section>
 	);
 }
